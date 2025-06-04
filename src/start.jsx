215b6aa099d50168781316427c84cc79a06c4431
Matthias Ang <ang_sen_luen_matthias@s2023.ssts.edu.sg>
2025-06-04 09:30:45 +0800

    trust

--- src/start.jsx
+++ src/start.jsx
@@ -17,38 +17,40 @@ export default function StartPage() {
   const { cipherText, name, toShow } = useMemo(() => {
     
     const groups = [
-  ["NATHAN LUKE GAMBOA CHONG JIA HAO", "KANNAN VITHAN", "Vegus Lim Zen Kng", "Dylan Nguyen Dang Khoa"],
-  ["RANDALL THAW ZAE TUN", "TANG KE HONG ZACHARY", "Wong Wei Kai Lucas", "Annamalai Palaniappan"],
-  ["XIE JINGXUAN", "GABRIEL LEE YU EN", "Sharlene Tan Qin Ying", "Gong Xiyue"],
-  ["Chen Ke Shih", "NEAH SHIBIL", "Adam Zafir bin Mohammed Sudhir", "Teiw Yong Sheng Jayden"],
-  ["Kang Jayl", "MASON TAN KOK YEOW", "Goh Rui Jie Thaddeus", "Aw Kai Bin, Zyden"],
-  ["LIM YI SHU LEEROY", "QUEK HYUN EE", "Amelia Ng", "Dundar Shin Berkin"],
-  ["TAN TECK JUN", "SEAN TAN BING XUAN", "LIM SHI HUI SERENE", "Lau Rei Yan Abigail"],
-  ["TRINITY HAN JIEXIN", "LE SON TUNG", "RONG ZHICHENG MICHAEL", "Joseph Kevin Fredric"],
-  ["LIM BO EN EDMUND", "Kyle Henric Buguina Chua", "CYRIL LIM JUN KAI", "Yang An Zhi"],
-  ["KANJIRAPARAMBIL MENON PARVATHY", "TANG KE WEI DAVEON", "PHUA SER YEN", "Lee Jun Lei Adam"],
-  ["POH JUN ZHE MATTHEW", "YAN BENG JIT BRYAN", "YONG JIA RUI EMMETT", "Alvarez Marco Lorenzo Tanzon"],
-  ["EZEKIEL ONG KAI BIN", "TAY AN ZHI WAYNE", "ENRIQUE KO KO", "Evan Tan jing kai"],
-  ["DRAYK TEH YUXIANG", "TEO ZHI KAI DARRYL", "Dessen Arwin Tan Ahn Joon", "Chuah Cheng Hang"],
-  ["TOH JUN CHEN", "HANA CHEE", "Chong Meng Fai", "Thiha Linn Tun"],
-  ["CHONG TANAKA KOUSUKE", "POTLURI MEDHANSH VEER", "GOH WEI KANG ISAAC", "Lai Hong Yu"],
-  ["Ivaniuk Aleksandr", "Scott Travis Jayden", "Ashton Lim Jing Kai", "Yeo Shu Axelia"],
-  ["OWEN SIAH", "MADALYN KERK YI LE", "Yip Jia Yi", "Goh Rae Pin"],
-  ["NAI WEI YE XAVIER", "NG XING YU SEBASTIAN", "ARUN KUMAR HEMANSRIKAR", "Maximilian Peter Waechter"],
-  ["SOO JIA QI ISAIAH", "ADHYA JAIN", "Lee Yu Zhe Travis", "Christopher Richard Mills"],
-  ["ADEN SOH TIAN TSAO", "ELI HUANG YUKAI", "MOKASHI AARUSH NINAD", "Aaden Ng"],
-  ["CHLOE LYNN ANNATASHYA SATYAWIJAYA", "ANG HAN SHENG WILFRED", "LEO MANSUKHANI", "Lim Zhe Hsien"],
-  ["Vishaakan Kumar", "Tan Yu xuan Astral", "OH KAIXU", "Javier Wee Jia Wei"],
-  ["LUIS ALEJANDRO VARGAS SIA", "TSANG ZI HENG", "Rafel Peh Zheng Yi", "Kenzo Gabriel Sutanto"],
-  ["KIREN KESHAUN PILLAI / ISHAN SREEJITH", "LEE DELONG MATTHIAS MARC", "WINSON CHAI", "Vishnu Kannappan Palaniappan"],
-  ["GOH XIN NI", "Lim Wen Hong", "Iresh Ramasamy", "Aldo Djojorahardjo"],
-  ["Phua Xuan Zhi Ethan", "Isaac Woon Xin Wei", "Chew Aorong Zavier", "Cyril Lim Jun Kai"]
+  ["NATHAN LUKE GAMBOA CHONG JIA HAO",       "KANNAN VITHAN",                  "Vegus Lim Zen Kng",                      "Dylan Nguyen Dang Khoa"],
+  ["RANDALL THAW ZAE TUN",                  "TANG KE HONG ZACHARY",           "Wong Wei Kai Lucas",                     "Annamalai Palaniappan"],
+  ["XIE JINGXUAN",                          "GABRIEL LEE YU EN",              "Sharlene Tan Qin Ying",                  "Yeo Shu Axelia"],
+  ["Chen Ke Shih",                          "NEAH SHIBIL",                    "Adam Zafir bin Mohammed Sudhir",         "Teiw Yong Sheng Jayden"],
+  ["Kang Jayl",                             "MASON TAN KOK YEOW",             "Goh Rui Jie Thaddeus",                   "Aw Kai Bin, Zyden"],
+  ["LIM YI SHU LEEROY",                     "QUEK HYUN EE",                   "Amelia Ng",                              "Dundar Shin Berkin"],
+  ["TAN TECK JUN",                          "SEAN TAN BING XUAN",             "LIM SHI HUI SERENE",                     "Lau Rei Yan Abigail"],
+  ["TRINITY HAN JIEXIN",                    "LE SON TUNG",                    "RONG ZHICHENG MICHAEL",                  "Joseph Kevin Fredric"],
+  ["LIM BO EN EDMUND",                      "Kyle Henric Buguina Chua",       "CYRIL LIM JUN KAI",                      "Yang An Zhi"],
+  ["KANJIRAPARAMBIL MENON PARVATHY",        "TANG KE WEI DAVEON",             "PHUA SER YEN",                           "Lee Jun Lei Adam"],
+  ["POH JUN ZHE MATTHEW",                   "YAN BENG JIT BRYAN",             "YONG JIA RUI EMMETT",                    "Alvarez Marco Lorenzo Tanzon"],
+  ["EZEKIEL ONG KAI BIN",                   "TAY AN ZHI WAYNE",               "ENRIQUE KO KO",                          "Evan Tan jing kai"],
+  ["DRAYK TEH YUXIANG",                     "TEO ZHI KAI DARRYL",             "Dessen Arwin Tan Ahn Joon",              "Chuah Cheng Hang"],
+  ["TOH JUN CHEN",                          "HANA CHEE",                      "Chong Meng Fai",                         "Thiha Linn Tun"],
+  ["CHONG TANAKA KOUSUKE",                  "POTLURI MEDHANSH VEER",          "GOH WEI KANG ISAAC",                     "Lai Hong Yu"],
+  ["Ivaniuk Aleksandr",                     "Scott Travis Jayden",            "Ashton Lim Jing Kai",                    "Gong Xiyue"],
+  ["OWEN SIAH",                             "MADALYN KERK YI LE",             "Yip Jia Yi",                             "Goh Rae Pin"],
+  ["NAI WEI YE XAVIER",                     "NG XING YU SEBASTIAN",           "ARUN KUMAR HEMANSRIKAR",                 "Maximilian Peter Waechter"],
+  ["SOO JIA QI ISAIAH",                     "ADHYA JAIN",                     "Lee Yu Zhe Travis",                      "Christopher Richard Mills"],
+  ["ADEN SOH TIAN TSAO",                    "ELI HUANG YUKAI",                "MOKASHI AARUSH NINAD",                   "Aaden Ng"],
+  ["CHLOE LYNN ANNATASHYA SATYAWIJAYA",     "ANG HAN SHENG WILFRED",          "LEO MANSUKHANI",                         "Lim Zhe Hsien"],
+  ["Vishaakan Kumar",                       "Tan Yu xuan Astral",             "OH KAIXU",                               "Javier Wee Jia Wei"],
+  ["LUIS ALEJANDRO VARGAS SIA",             "TSANG ZI HENG",                  "Rafel Peh Zheng Yi",                     "Kenzo Gabriel Sutanto"],
+  ["KIREN KESHAUN PILLAI / ISHAN SREEJITH", "LEE DELONG MATTHIAS MARC",       "WINSON CHAI",                            "Vishnu Kannappan Palaniappan"],
+  ["GOH XIN NI & KYAL TARYAR LYNN MYAT @ JASPER LYNN MYAT", "Lim Wen Hong",   "Iresh Ramasamy",               "Aldo Djojorahardjo"],
+  ["Phua Xuan Zhi Ethan & Kai Jonas Theolke","Isaac Woon Xin Wei","Chew Aorong Zavier",             "Kingsley"],
+  ["Chin Wei Xin",                          "Astral",                         "Rayden",                                 "Rayson"],
+  ["Elva",                                  "kayden",                 "Dylan",                           "Balla"],
+  ["Ethan",                          "Yu Xiang",                "wan hou",                         "Kaixu"]
 ];
     const key = groups[groupnumber - 1]?.[Math.floor(Math.random() * 4)].trim() || "";
-    const ct = String("INCAMP" + makeid(key.length)).toUpperCase();
+    const ct = String("INCAMP" + makeid(key.length*2-6)).toUpperCase();
     const vig = Vigenereencrypt(ct, key);
     return {
-
       cipherText: ct,
       name: key,
       toShow: text2Binary(vig)
@@ -73,10 +75,12 @@ export default function StartPage() {
   }, [success]);
 
   function showHintFunc() {
-    if (showHint > 300) {
-      showActualHInt(1);
-    }
+  if (showHint >= 300) {
+    alert(name)
+  } else {
+    alert(`Hint available in ${300 - showHint} seconds`)
   }
+}
   const showActualHInt = () => {
     alert(name)
   }
